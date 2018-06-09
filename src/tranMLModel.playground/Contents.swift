fe4eb180c0c43dac121e72f4e4475185ea54808e
Yuta Yamamoto <tuki0918@gmail.com>
2018-06-09 17:47:01 +0900

    Mac GUIでCNNを試すスクリプト（Xcode 10+ beta）

---
+++ src/tranMLModel.playground/Contents.swift
@@ -0,0 +1,7 @@
+// https://developer.apple.com/documentation/create_ml/mlimageclassifierbuilder
+// dataset: https://github.com/myleott/mnist_png
+
+import CreateMLUI
+
+let builder = MLImageClassifierBuilder()
+builder.showInLiveView()
